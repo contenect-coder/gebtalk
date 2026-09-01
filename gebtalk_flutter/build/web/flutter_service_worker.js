1a54e59b6281e0e9cdcca85e1e7087e215a48af4
John Franklin Victor <johnfranklinvictor@users.noreply.github.com>
2026-09-01 13:45:35 +0530

    Deploy latest Flutter web release to Netlify

--- gebtalk_flutter/build/web/flutter_service_worker.js
+++ gebtalk_flutter/build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7ebcc7c59f3b00892c5620b9ee15c1f4
 "canvaskit/wimp.wasm": "bd9945e051eaff19b80c59dca4f00e66",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "71ff8451f0c1f0dba4590aa9c563f616",
+"flutter_bootstrap.js": "2c97a90abdb41632b0d5dea573b13f7c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
